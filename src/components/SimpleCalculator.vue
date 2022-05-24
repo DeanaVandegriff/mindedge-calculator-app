867164507ed0510c35d3fda97a596c0efffb5867
deana <dvandegriff1@gmail.com>
2022-05-24 11:43:45 -0500

    replaces HelloWorld component with SimpleCalculator component and installs bootstrap

---
+++ src/components/SimpleCalculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Calculators Rock</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SimpleCalculator",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SimpleCalculator></SimpleCalculator>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SimpleCalculator from "@/components/SimpleCalculator.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    SimpleCalculator,
   },
 };
 </script>
